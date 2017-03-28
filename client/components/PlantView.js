bf2998396ecf1b187dcf21cdac0e3cea2f76274a
JessicaNeary <Caneary@hotmail.com>
2017-03-29 11:35:57 +1300

    PlantView page now shows details of current plant

--- client/components/PlantView.js
+++ client/components/PlantView.js
@@ -6,12 +6,24 @@ export default React.createClass({
   },
   render () {
     const plant = this.props.plant
-    const details = plant ? plant.name : null
-    return (
-      <div>
-        <h3>Plants</h3>
-        <h4>{details}</h4>
+    const otherName = plant && plant.other_name ? <li>Also known as a {plant.other_name.toLowerCase()}</li> : null
+    const details =
+      plant
+      ? <div className='details'>
+        <h3>{plant.name}</h3>
+        <ul>
+          <li>{plant.scientific_name}</li>
+          {otherName}
+          <li>{plant.type}</li>
+          <li>Harvest in {plant.harvest}</li>
+          <li>Sowing distance: {plant.sowing_distance}</li>
+          <li>Ideal tempreture: {plant.ideal_tempreture}</li>
+        </ul>
+        <div>{plant.instructions}</div>
       </div>
+      : <div>Nothing found</div>
+    return (
+      details
     )
   }
 })
