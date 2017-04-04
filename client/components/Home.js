a0a5814632aacaf9eb6f7d9fb968fff119111200
JessicaNeary <Caneary@hotmail.com>
2017-04-04 12:15:23 +1200

    Homepage rendering basic plant list, d3 installed

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,19 +1,26 @@
 import React from 'react'
 
+var d3 = require("d3")
+
 export default React.createClass({
   componentDidMount () {
     this.props.allPlants()
+
   },
   render () {
+    var body = d3.select("body")
+    var div = body.append("div")
+    div.html("Hello, world!")
     const plants = this.props.plants
-    const plantList = plants
-      ? plants.map(plant => {
-        <div>{plant.name}</div>
-      }) : null
+    const list = plants ? plants.map(plant => {
+                  return  <div key={plant.id}>{plant.name}</div>
+                  }) : <p>No data found</p>
     return (
       <div>
         <h4>Home</h4>
-        <div>{plantList}</div>
+        <div>
+        {list}
+        </div>
       </div>
     )
   }
