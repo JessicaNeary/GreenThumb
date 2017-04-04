e8878aa48806e591fc3678ff6bcf6ac03ef1c8a6
JessicaNeary <Caneary@hotmail.com>
2017-04-05 10:50:08 +1200

    d3 bars displaying with mock data'

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,25 +1,61 @@
 import React from 'react'
 
-var d3 = require("d3")
+var d3 = require('d3')
 
 export default React.createClass({
   componentDidMount () {
     this.props.allPlants()
-
   },
   render () {
-    var body = d3.select("body")
-    var div = body.append("div")
-    div.html("Hello, world!")
-    const plants = this.props.plants
-    const list = plants ? plants.map(plant => {
-                  return  <div key={plant.id}>{plant.name}</div>
-                  }) : <p>No data found</p>
+    const data = [
+      {
+        name: 'cabbage',
+        seasons: [
+          {id: 1},
+          {id: 2},
+          {id: 4},
+          {id: 5}
+        ]
+      },
+      {
+        name: 'carrot',
+        seasons: [
+          {id: 1},
+          {id: 3},
+          {id: 4},
+          {id: 6}
+        ]
+      }
+    ]
+
+    d3.select('.content').append('div')
+      .selectAll('svg')
+      .data(data)
+      .enter()
+      .append('svg')
+      .attr('width', 500)
+      .attr('height', 100)
+      .append('text')
+      .text(function (d) {
+        return d.name
+      })
+      .attr('y', 10)
+    d3.selectAll('svg').selectAll('rect')
+        .data(function (d) {
+          return d.seasons
+        })
+        .enter()
+        .append('rect')
+        .attr('width', 40)
+        .attr('height', 20)
+        .attr('x', function (d) {
+          return 15 + d.id * 40
+        })
+        .style('fill', 'green')
     return (
       <div>
         <h4>Home</h4>
-        <div>
-        {list}
+        <div className='content'>
         </div>
       </div>
     )
